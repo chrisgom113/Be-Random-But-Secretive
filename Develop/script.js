e4de3a17871df2c393d4799172820c4573a8b0e3
chrisgom113 <cgomez4@uci.edu>
2023-01-16 21:42:05 -0800

    logical conditions for character case inputs

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,61 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
 
-  var chooseLength = window.prompt("How long would you like your password to be? (Enter a number value: 8-128)");
+  // Logical Conditions for Character Length Inputs
+
+  var chooseLength = window.prompt("How many characters? (Enter value between 8-128)");
 
   if (chooseLength === null) {
     console.log(chooseLength);
     return;
-  
-  
-  } else if(chooseLength === "") {
+
+
+  } else if (chooseLength === "") {
     window.alert("You must enter a value! Please try again");
     writePassword();
 
   } else if (
     (chooseLength < 8) ||
     (chooseLength > 128)) {
-    window.alert("The number value you've entered is either too small or too large. Please enter a value within 8-128");
+    window.alert("The value entered is either too small, too large, or not a number. Please enter a value within 8-128");
     writePassword();
 
-  }  
+  } else if (chooseLength >= 8 && chooseLength <= 128) {
+    console.log(chooseLength);
+    window.alert("Thank you. Your password will be a length of " + chooseLength + " characters.");
 
+  }
 
+  // Logical Conditions for Character Case Inputs
+
+  var letterCasing = window.prompt("Do you want your password to have: uppercase, lowercase, or both? Enter 'U', 'L', or 'B'");
+
+  if (letterCasing === null) {
+
+    window.alert("Darn! You'll have to start from the beginning again");
+    return;
+  }
+  
+  letterCasing = letterCasing.toUpperCase();
+
+  if (letterCasing === "") {
+    window.alert("You must enter 'U', 'L', or 'B' to continue! Please try again");
+    letterCasing = window.prompt("Do you want your password to have: uppercase, lowercase, or both? Enter 'U', 'L', or 'B'");
+
+  } else if (letterCasing == "U") {
+    window.alert("Thank you. Your password will contain all UPPERCASE characters.");
+  } else if (letterCasing == "L") {
+    window.alert("Thank you. Your password will contain all lowercase characters.");
+  } else if (letterCasing == "B") {
+    window.alert("Thank you. Your password will contain both upper and lower case characters.");
+  }
+
+  var numeric = window.prompt("Do you want numbers in your password? Enter 'Y' or 'N'")
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
